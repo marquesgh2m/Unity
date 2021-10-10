57d932a9063c236c6764ce32c31625cf29219229
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2021-10-10 15:53:26 -0300

    clean code

--- examples/sort/src/sort.c
+++ examples/sort/src/sort.c
@@ -27,25 +27,6 @@ void bs(int n, int * vetor){
 }
 
 void sort(int * vector, int vector_size){
-    //qsort(vector, vector_size, sizeof(int), cmpfunc);
     bs(vector_size, vector);
+    //qsort(vector, vector_size, sizeof(int), cmpfunc);
 }
-
-// BKP
-// Algoritmo do BubbleSort
-// void bs(int n, int * vetor){
-//     int c=0, d, troca, trocou =1;
-
-//     while (c < (n-1) & trocou ){
-//         trocou = 0;
-//         for (d = 0 ; d < n - c - 1; d++){
-//             if (vetor[d] > vetor[d+1]){
-//                 troca      = vetor[d];
-//                 vetor[d]   = vetor[d+1];
-//                 vetor[d+1] = troca;
-//                 trocou = 1;
-//             }
-//         }
-//         c++;
-//     }
-// }
