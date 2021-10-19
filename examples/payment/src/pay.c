fc97d3f018b4a22e35e9b19e5ac9d1191ab04b0d
marquesgh2m <guilherme.marques.002@acad.pucrs.br>
2021-10-18 23:53:34 -0300

    funções e testes implementados

--- examples/payment/src/pay.c
+++ examples/payment/src/pay.c
@@ -4,5 +4,11 @@
 #include <stdlib.h>
 
 int payment(float value, char status[15]){
+    if(value < 0.009 || value > 99999.00) return 1;
+    if (strcmp(status, "regular")!=0 &&
+        strcmp(status, "estudante")!=0 &&
+        strcmp(status, "aposentado")!=0 &&
+        strcmp(status, "VIP")!=0) return 2;
     return 0;
 }
+
